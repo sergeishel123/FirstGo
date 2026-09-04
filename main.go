23ad3e96e0d38ce6f32514fe856d7267e7e3b02d
sergeishel123 <97154242+sergeishel123@users.noreply.github.com>
2026-09-04 15:33:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+
+    t1 := time.Now()
+    t2 := time.Date(t1.Year() + 1, time.January, 1,0,0,0,0, time.UTC)
+
+    delta := t2.Sub(t1)
+
+    days := int(delta.Hours() / 24)
+
+	fmt.Printf("До Нового года осталось %d дней", days)
+
+
+}
 
