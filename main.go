421a263410c721ccc30811c77e9f5b48f3e1af95
sergeishel123 <97154242+sergeishel123@users.noreply.github.com>
2026-09-25 14:14:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// Возвращает кол-во дней до нового года (к тому же так как функция публичная, то с большой буквы)
 func DaysToNewYear(t time.Time) int {
 	t2 := time.Date(t.Year()+1, time.January, 1, 0, 0, 0, 0, time.UTC)
 	delta := t2.Sub(t)
