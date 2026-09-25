07bc0d29311f077403962798848dfaf4460732ef
sergeishel123 <97154242+sergeishel123@users.noreply.github.com>
2026-09-25 15:31:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json" // для JSON ответов
+	"net/http"      // HTTP -  сервер
 	"time"
 )
 
@@ -12,11 +13,56 @@ func DaysToNewYear(t time.Time) int {
 	return int(delta.Hours() / 24)
 }
 
+// Новый обработчик HTTP-запросов (Шаг 5 задания)
+func apiHandler(Response http.ResponseWriter, Request *http.Request) {
+
+	// Дату выцепляю как параметр Get-запроса date
+	dateInQuery := Request.URL.Query().Get("date")
+	var date time.Time
+	var err error
+
+	//	Определяем дату для расчета, если пустая, то текущую берём
+	if dateInQuery == "" {
+		date = time.Now()
+	} else {
+
+		// Пытаюсь распарсить дату в классическом виде
+
+		date, err = time.Parse("2026-09-25", dateInQuery)
+		if err != nil { // Я наконец понял, что функции в Go зачастую возвращают вторым результатом ошибку. Если она не пустая(не nil), то плохо
+
+			// Если формат неверный, возвращаем и JSON с ошибкой, статус response 400
+			Response.Header().Set("Content-Type", "application/json")
+			Response.WriteHeader(http.StatusBadRequest)
+
+			json.NewEncoder(Response).Encode(map[string]string{"Ошибка": "Неправильный формат даты, используй формат  YYYY-MM-DD"})
+			return
+
+		}
+	}
+
+	days := DaysToNewYear(date)
+
+	// Если без ошибочно, тол json - ом вовзращаем ответ со статусом 200
+	Response.Header().Set("Content-Type", "application/json")
+	Response.WriteHeader(http.StatusOK)
+
+	MyResponse := map[string]interface{}{"Количество дней до Нового Года": days}
+	if dateInQuery != "" {
+		MyResponse["date"] = dateInQuery
+	}
+
+	json.NewEncoder(Response).Encode(MyResponse)
+}
+
+// Endpoint по идее
 func main() {
 
-	t := time.Now()
+	// Обработчик Get - запросов сделал по пути SergeyAndDays
+	http.HandleFunc("/SergeyAndDays", apiHandler)
 
-	days := DaysToNewYear(t)
+	// Теперь по пути http://localhost:8080/SergeyAndDays по идее можно обращаться для расчёта кол-ва дней
 
-	fmt.Printf("До Нового года осталось %d дней\n", days)
+	// Запуск сервера локально  (порт 8080)
+	http.ListenAndServe(":8080", nil)
 }
